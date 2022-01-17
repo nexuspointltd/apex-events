49766cea52016be969311e8c1d3b24738eae3d4b
Paul Winter <paul.winter@nexuspoint.co.uk>
2022-01-17 21:11:15 +0000

    Updated log export to include additional fields.

--- app/Broadcasts/LogSmash/Export/LogExportBroadcast.php
+++ app/Broadcasts/LogSmash/Export/LogExportBroadcast.php
@@ -26,27 +26,82 @@ class LogExportBroadcast extends AbstractBroadcast
     public $timestamp;
 
     /**
+     * @var string
+     */
+    public $model_type;
+
+    /**
+     * @var string
+     */
+    public $model_id;
+
+    /**
+     * @var string
+     */
+    public $method;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $dealer_id;
+
+    /**
+     * @var int
+     */
+    public $response_code;
+
+    /**
+     * @var string
+     */
+    public $response_body;
+
+    /**
      * @var array
      */
     public $tenant;
 
     /**
-     * LogExportBroadcast constructor.
      * @param string $guid
      * @param string $environment
      * @param string $timestamp
-     * @param array  $tenant
+     * @param string $model_type
+     * @param string $model_id
+     * @param string $method
+     * @param string $type
+     * @param string $dealer_id
+     * @param int $response_code
+     * @param string $response_body
+     * @param array $tenant
      */
     public function __construct(
         string $guid,
         string $environment,
         string $timestamp,
+        string $model_type,
+        string $model_id,
+        string $method,
+        string $type,
+        string $dealer_id,
+        int $response_code,
+        string $response_body,
         array $tenant
     ) {
-        $this->guid        = $guid;
-        $this->environment = $environment;
-        $this->timestamp   = $timestamp;
-        $this->tenant      = $tenant;
+        $this->guid             = $guid;
+        $this->environment      = $environment;
+        $this->timestamp        = $timestamp;
+        $this->model_type       = $model_type;
+        $this->model_id         = $model_id;
+        $this->method           = $method;
+        $this->type             = $type;
+        $this->dealer_id        = $dealer_id;
+        $this->response_code    = $response_code;
+        $this->response_body    = $response_body;
+        $this->tenant           = $tenant;
     }
 
     /**
--- app/Events/LogSmash/Export/LogExportEvent.php
+++ app/Events/LogSmash/Export/LogExportEvent.php
@@ -27,28 +27,82 @@ class LogExportEvent extends AbstractEvent implements LogSmashEventInterface
     public $timestamp;
 
     /**
+     * @var string
+     */
+    public $model_type;
+
+    /**
+     * @var string
+     */
+    public $model_id;
+
+    /**
+     * @var string
+     */
+    public $method;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $dealer_id;
+
+    /**
+     * @var int
+     */
+    public $response_code;
+
+    /**
+     * @var string
+     */
+    public $response_body;
+
+    /**
      * @var array
      */
     public $tenant;
 
     /**
-     * LogExportEvent constructor.
      * @param string $guid
      * @param string $environment
      * @param string $timestamp
-     * @param array  $tenant
-     * @noinspection DuplicatedCode
+     * @param string $model_type
+     * @param string $model_id
+     * @param string $method
+     * @param string $type
+     * @param string $dealer_id
+     * @param int $response_code
+     * @param string $response_body
+     * @param array $tenant
      */
     public function __construct(
         string $guid,
         string $environment,
         string $timestamp,
+        string $model_type,
+        string $model_id,
+        string $method,
+        string $type,
+        string $dealer_id,
+        int $response_code,
+        string $response_body,
         array $tenant
     ) {
-        $this->guid        = $guid;
-        $this->environment = $environment;
-        $this->timestamp   = $timestamp;
-        $this->tenant      = $tenant;
+        $this->guid             = $guid;
+        $this->environment      = $environment;
+        $this->timestamp        = $timestamp;
+        $this->model_type       = $model_type;
+        $this->model_id         = $model_id;
+        $this->method           = $method;
+        $this->type             = $type;
+        $this->dealer_id        = $dealer_id;
+        $this->response_code    = $response_code;
+        $this->response_body    = $response_body;
+        $this->tenant           = $tenant;
     }
 
     /**
@@ -57,10 +111,17 @@ class LogExportEvent extends AbstractEvent implements LogSmashEventInterface
     public function getMetaData(): array
     {
         return [
-            'guid'        => $this->guid,
-            'environment' => $this->environment,
-            'timestamp'   => $this->timestamp,
-            'tenant'      => $this->tenant
+            'guid'          => $this->guid,
+            'environment'   => $this->environment,
+            'timestamp'     => $this->timestamp,
+            'model type'    => $this->model_type,
+            'model_id'      => $this->model_id,
+            'method'        => $this->method,
+            'type'          => $this->type,
+            'dealer ID'     => $this->dealer_id,
+            'response_code' => $this->response_code,
+            'response_body' => $this->response_body,
+            'tenant'        => $this->tenant
         ];
     }
 
