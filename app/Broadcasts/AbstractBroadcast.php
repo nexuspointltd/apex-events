5a9280fc8b0a844c240962c4d29313539e1cca40
Harry Kirkman <harry.kirkman@nexuspoint.co.uk>
2023-04-13 10:51:37 +0100

    APX-7103 force broadcaster to use redis, to prevent conflicts with sockets

--- app/Broadcasts/AbstractBroadcast.php
+++ app/Broadcasts/AbstractBroadcast.php
@@ -3,11 +3,23 @@
 namespace Nexus\ApexEvents\Broadcasts;
 
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\InteractsWithBroadcasting;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 abstract class AbstractBroadcast implements ShouldBroadcast
 {
     use InteractsWithSockets;
+    use InteractsWithBroadcasting;
     use SerializesModels;
+
+    /**
+     * Override the broadcaster driver to use redis, as the trait's property cannot be overriden by the
+     * implementing class.
+     * @return string[]
+     */
+    public function broadcastConnections()
+    {
+        return ['redis'];
+    }
 }
